7d582a383b79945861a67e64faf689ef0282c078
meryembenassila <benassila.meryem08@gmail.com>
2026-03-04 18:09:44 +0000

    intial commit

---
+++ src/main/java/com/example/tp_springmvcspringdata_jpahibernate/TpSpringMvcSpringDataJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp_springmvcspringdata_jpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringMvcSpringDataJpaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpSpringMvcSpringDataJpaHibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tp_springmvcspringdata_jpahibernate/TpSpringMvcSpringDataJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp_springmvcspringdata_jpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringMvcSpringDataJpaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
