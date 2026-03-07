89900717f0070d5c51df3f1b32035061cb9d0c05
meryembenassila <benassila.meryem08@gmail.com>
2026-03-07 10:38:56 +0000

    créer contrôleur Spring MVC + vues Thymeleaf pour lister, ajouter et supprimer des produits avec validation et layout Bootstrap

---
+++ src/main/java/com/example/tp_springmvcspringdata_jpahibernate/web/ProductController.java
@@ -0,0 +1,59 @@
+package com.example.tp_springmvcspringdata_jpahibernate.web;
+
+import com.example.tp_springmvcspringdata_jpahibernate.entities.Product;
+import com.example.tp_springmvcspringdata_jpahibernate.repository.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+
+
+@Controller
+public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+    @GetMapping("/index")
+    public String index(Model model){
+        List<Product> products =  productRepository.findAll();
+        model.addAttribute("productsList",products);
+        return  "products";
+
+    }
+
+
+    @GetMapping("/")
+    public String Home(){
+        return  "redirect:/index";
+
+    }
+    @GetMapping("/delete")
+    public String delete(@RequestParam(name = "id") Long id){//il va chercher dans l'url un parametre id requete para c'est si ce id a un autre nom
+        productRepository.deleteById(id);
+        return "redirect:/index";//redirection
+}
+    @GetMapping("/newproduct")
+    public String newproduct(Model model){
+       model.addAttribute("product",new Product());
+       return  "newProduct";//forword
+
+    }
+
+    @PostMapping("/saveProduct")
+    public String saveProduct(@Valid Product product , BindingResult bindingResult ){
+        // @Valid sert à activer la validation des champs d’un objet sinon il ne va pas les vérifier
+        // BindingResult récupère les erreurs.
+        if (bindingResult.hasErrors()){
+            return  "newProduct";
+        }
+        productRepository.save(product);
+        return  "redirect:/index";//forword
+
+    }
+}
